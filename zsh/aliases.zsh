f19620e6a68cdfa8b10cd053bc22d11e87b1d919
Georg Schwarz <georg.schwarz@fau.de>
2019-04-03 10:00:17 +0200

    Adapt aliases to my needs

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -16,19 +16,17 @@ alias ....='cd ../../..'
 alias .....='cd ../../../..'
 
 # System Maintainence
-alias upd='trizen -Syyu && flatpak update'
-alias p='sudo pacman --color auto'
-alias t='trizen'
+alias upd='pacman -Syyu && yay -Syyu'
 alias progs="(pacman -Qet && pacman -Qm) | sort -u" # List programs I've installed
 alias orphans="pacman -Qdt" # List orphan programs
-alias sdn="sudo shutdown now"
 alias mirrors='sudo reflector --score 100 --fastest 25 --sort rate --save /etc/pacman.d/mirrorlist --verbose'
 	
 # Make mount command readable
 alias mount='mount |column -t'
 
 # Important Directories
-alias dev='cd ~/dev'
+alias develop='cd ~/development'
+alias ods='cd ~/ods'
 
 # GIT
 alias gs='git status '
