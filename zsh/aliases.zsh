28cece988fd4a64776b595e2b020e391d5407c9e
Georg Schwarz <dev@georg-schwarz.com>
2020-12-21 15:59:03 +0100

    Remove unused stuff

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,10 +4,7 @@ unalias -a
 
 # List 
 alias ls='ls --color=auto'
-alias ll='ls -l'
-alias l.='ls -d .* --color=auto'
-alias ll.='ls -la'
-alias k='k -h --no-vcs'
+alias ll='ls -la --color=auto'
 
 # Easier navigation
 alias ..='cd ..'
@@ -19,14 +16,6 @@ alias .....='cd ../../../..'
 alias upd='pacman -Syyu && yay -Syyu'
 alias progs="(pacman -Qet && pacman -Qm) | sort -u" # List programs I've installed
 alias orphans="pacman -Qdt" # List orphan programs
-alias mirrors='sudo reflector --score 100 --fastest 25 --sort rate --save /etc/pacman.d/mirrorlist --verbose'
-	
-# Make mount command readable
-alias mount='mount |column -t'
-
-# Important Directories
-alias develop='cd ~/development'
-alias ods='cd ~/ods'
 
 # GIT
 alias gs='git status '
@@ -34,7 +23,7 @@ alias ga='git add '
 alias gb='git branch '
 alias gc='git commit --verbose'
 alias gd='git diff'
-alias gco='git checkout '
+alias gch='git checkout '
 alias gl='git log --graph --decorate --pretty=oneline --abbrev-commit'
 
 # Restart Plasmashell (due to bug KDE freezes)
--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -32,30 +32,5 @@ extract() {
 
 # `ls` after `cd`
 function cd {
-	builtin cd "$@" && ls -F
-}
-
-# Determine size of a file or total size of a directory
-# Credit: https://github.com/jessfraz/dotfiles/blob/master/.functions
-fs() {
-	if du -b /dev/null > /dev/null 2>&1; then
-		local arg=-sbh
-	else
-		local arg=-sh
-	fi
-	# shellcheck disable=SC2199
-	if [[ -n "$@" ]]; then
-		du $arg -- "$@"
-	else
-		du $arg -- .[^.]* *
-	fi
-}
-
-# `tre` is a shorthand for `tree` with hidden files and color enabled, ignoring
-# the `.git` directory, listing directories first. The output gets piped into
-# `less` with options to preserve color and line numbers, unless the output is
-# small enough for one screen.
-# Credit: https://github.com/jessfraz/dotfiles/blob/master/.functions
-tre() {
-	tree -aC -I '.git' --dirsfirst "$@" | less -FRNX
+	builtin cd "$@" && ll
 }
